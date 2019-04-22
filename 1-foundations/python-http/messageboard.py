64b5757b8bb911923e95df2d6888725f1bde3c88
Michael A. Klopfer <mike.klopfer@outlook.com>
2019-04-23 02:01:41 +0800

    Messageboard exercise complete, final edits

--- 1-foundations/python-http/messageboard.py
+++ 1-foundations/python-http/messageboard.py
@@ -23,6 +23,8 @@
 import http.server as hs
 import urllib
 
+memory = []
+
 msg_html = '''
 <!DOCTYPE html>
   <title>Message Board</title>
@@ -35,27 +37,49 @@ msg_html = '''
 
 class MessageHandler(hs.BaseHTTPRequestHandler):
     def do_POST(self):
-        # 1. How long was the message? (Use the Content-Length header.)
+        # How long was the message? (Use the Content-Length header.)
         length = int(self.headers.get('Content-Length', 0))
 
-        # 2. Read the correct amount of data from the request.
+        # Read the correct amount of data from the request.
         data = self.rfile.read(length).decode()
 
-        # 3. Extract the "message" field from the request data.
+        # Extract the "message" field from the request data.
         message = urllib.parse.parse_qs(data)['message'][0]
-        
+
+        # Escape HTML tags in the message so users can't break world
+        message = message.replace("<", "&lt;")
+
+        # Store it in memory.
+        memory.append(message)
+
+        # 1. Send a 303 redirect back to the root page.
+        self.send_response(303)
+        self.send_header('Content-type', 'text/HTML; charset=utf-8')
+        self.send_header('Location', '/')
+        self.end_headers
+
         # Send the "message" field back as the response.
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
         self.wfile.write(message.encode())
-
+    
     def do_GET(self):
+        # First, send a 200 OK response.
         self.send_response(200)
-        self.send_header('Content-type', 'text/HTML; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(msg_html.encode())
 
+        # Then send headers.
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        # 2. Put the response together out of the form and the stored messages.
+        full_msg = ''
+        for msg in memory:
+          full_msg = full_msg + '\n' + msg
+        
+        full_msg = full_msg + msg_html
+        # 3. Send the response.
+        self.wfile.write(full_msg.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
